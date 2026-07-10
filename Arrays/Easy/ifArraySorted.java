a94adcb143681a991a3e1537eb8ab4a8b6543904
Mudasir Talpur <Mirmudassiir@gmail.com>
2026-07-10 22:32:49 +0500

    Check if Array is Sorted

---
+++ Arrays/Easy/ifArraySorted.java
@@ -0,0 +1,20 @@
+// package Arrays.Easy;
+
+public class ifArraySorted{
+    public void main(String[] args){
+        int[] nums = {4, 6, 8, 9, 13, 65};
+        if(isSorted(nums))
+            System.out.print("The given Array is Soretd.");
+        else
+            System.out.print("The given Array is not Sorted.");
+    }
+
+    public boolean isSorted(int[] nums){
+        boolean sortedOrNot = true;
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] > nums[i+1])
+                sortedOrNot = false;
+        }
+        return sortedOrNot;
+    }
+}
