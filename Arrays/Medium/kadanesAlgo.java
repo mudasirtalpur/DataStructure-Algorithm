34c59f4d40ac8ed4a31c6db8939fd5c127aae7ba
Mudasir Talpur <Mirmudassiir@gmail.com>
2026-07-17 23:09:28 +0500

    Gets the max SubArray Sum

---
+++ Arrays/Medium/kadanesAlgo.java
@@ -0,0 +1,24 @@
+// package Arrays.Medium;
+
+public class kadanesAlgo{
+    public void main(String[] args){
+        int[] nums = {-1};
+
+        System.out.print(maxSubArray(nums));
+
+    }
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int j = 0; j < nums.length; j++){
+            sum += nums[j];
+            maxSum = Math.max(maxSum, sum);
+            if(sum < 0)
+                sum = nums[j];
+
+        }
+
+        return maxSum;
+    }
+}
