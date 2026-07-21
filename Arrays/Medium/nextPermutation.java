0c507b5b5eac9d84691ee5822a7db4e90bf01a5f
Mudasir Talpur <Mirmudassiir@gmail.com>
2026-07-21 20:54:43 +0500

    Finds the next permutation of given array if last permutation done then sort it

---
+++ Arrays/Medium/nextPermutation.java
@@ -0,0 +1,52 @@
+// package Arrays.Medium;
+
+public class nextPermutation{
+    public void main(String[] args){
+        int[] nums = {1,1,5};
+
+        permutation(nums);
+
+        System.out.println("Next permutation of given is: ");
+        for(int num : nums)
+            System.out.print(num + " ");
+    }
+
+    public void permutation(int[] nums){
+     int index = -1;
+
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1){
+            reverse(nums, 0, nums.length-1);
+            return;
+        }
+
+        for(int i = nums.length-1; i >= 0; i--){
+            if(nums[i] > nums[index]){
+                swap(nums, i, index);
+                break;
+            }
+        }   
+
+        reverse(nums, index+1, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
